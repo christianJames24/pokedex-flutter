2623ae93efe6efe68a828cc735d05bf1cea94ab7
piplupcool123 <christianvehicle@gmail.com>
2025-12-31 14:31:55 -0500

    Update string_extension.dart

--- lib/data/string_extension.dart
+++ lib/data/string_extension.dart
@@ -4,7 +4,7 @@
 
 extension StringExtension on String {
   String capitalize() {
-    return "${this[0].toUpperCase()}${this.substring(1).toLowerCase()}";
+    return "${this[0].toUpperCase()}${substring(1).toLowerCase()}";
   }
 
   String capitalize2() {
